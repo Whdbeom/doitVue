2b67b87fb2544fa73cd77322f4c2fb18f88cb64e
jbPark <whdqja918@gmail.com>
2024-09-23 14:43:15 +0900

    no message

--- cli/src/App.vue
+++ cli/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="app">
     <TodoHeader/>
-    <TodoInput/>
-    <TodoList/>
-    <TodoFooter/>
+    <TodoInput v-on:addTodo="addTodo"/>
+    <TodoList v-bind:propsdata="todoItems" @removeTodo="removeTodo"/>
+    <TodoFooter v-on:removeAll="clearAll"/>
   </div>
 </template>
 
@@ -14,7 +14,38 @@ import TodoList from './components/TodoList.vue';
 import TodoFooter from './components/TodoFooter.vue';
 
 export default {
-  name: 'App',
+  data(){
+    return{
+      todoItems: []
+    }
+  },
+  
+  created() {
+    if(localStorage.length > 0){
+      for(let i =0; i < localStorage.length; i++){
+        this.todoItems.push(localStorage.key(i));
+      }
+    }
+  },
+
+  methods: {
+    addTodo(todoItem){
+      localStorage.setItem(todoItem, todoItem);
+      this.todoItems.push(todoItem);
+    },
+
+    clearAll(){
+      localStorage.clear();
+      this.todoItems = [];
+    },
+
+    removeTodo(todoItem, index){
+      localStorage.removeItem(todoItem);
+      this.todoItems.splice(index, 1);
+    }
+  },
+
+  // name: 'App',
   components: {
     TodoHeader,
     TodoInput,
--- cli/src/components/TodoInput.vue
+++ cli/src/components/TodoInput.vue
@@ -4,23 +4,36 @@
       <span class="addContainer" v-on:click="addTodo">
         <i class="addBtn fas fa-plus" aria-hidden="true"></i>
       </span>
+
+      <TodoModal v-if="showModal" @close="showModal = false">
+        <h3 slot="header">경고</h3>
+        <span slot="footer" @click="showModal = false">
+          할 일을 입력하세요.
+          <i class="closeModalBtn fas fa-times" aria-hidden="true"></i>
+        </span>
+      </TodoModal>
     </div>
   </template>
   
   <script>
+import TodoModal from './common/TodoModal.vue';
   export default {
 
     data(){
       return {
-        newTodoItem: ''
+        newTodoItem: '',
+        showModal: false
       }
     },
     methods: {
       addTodo(){
         if(this.newTodoItem !== ""){
           const value = this.newTodoItem && this.newTodoItem.trim();
-          localStorage.setItem(value, value);
+          // localStorage.setItem(value, value);
+          this.$emit('addTodo', value);
           this.clearInput();
+        } else {
+          this.showModal = !this.showModal;
         }
       },
 
@@ -29,6 +42,10 @@
       }
 
 
+    },
+
+    components: {
+      TodoModal
     }
   }
   </script>
